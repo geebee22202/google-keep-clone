d54df8e94ad7b2c9b51cd8c035f7dd2177fd41f9
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-04-10 11:57:54 -0400

    added functionality to close button on form

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ class App {
     this.$noteTitle = document.querySelector("#note-title");
     this.$noteText = document.querySelector("#note-text");
     this.$formButtons = document.querySelector("#form-buttons");
+    this.$formCloseButton = document.querySelector("#form-close-button");
+
     this.addEventListeners();
   }
   addEventListeners() {
@@ -19,20 +21,29 @@ class App {
       event.preventDefault();
       const title = this.$noteTitle.value;
       const text = this.$noteText.value;
-
       const hasNote = title || text;
 
       if (hasNote) {
         this.addNote({ title, text });
       }
     });
+
+    this.$formCloseButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+      this.closeForm();
+    });
   }
 
   handleFormClick(event) {
     const isFormClicked = this.$form.contains(event.target);
+    const title = this.$noteTitle.value;
+    const text = this.$noteText.value;
+    const hasNote = title || text;
 
     if (isFormClicked) {
       this.openForm();
+    } else if (hasNote) {
+      this.addNote({ title, text });
     } else {
       this.closeForm();
     }
@@ -52,10 +63,10 @@ class App {
     this.$noteText.value = "";
   }
 
-  addNote(note) {
+  addNote({ title, text }) {
     const newNote = {
-      title: note.title,
-      text: note.text,
+      title: title,
+      text: text,
       color: "white",
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
     };
@@ -68,8 +79,9 @@ class App {
     const hasNotes = this.notes.length > 0;
     this.$placeholder.style.display = hasNotes ? "none" : "flex";
 
-    this.$notes.innerHTML = this.notes.map(
-      (note) => `
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) => `
       <div style="background: ${note.color};" class="note">
         <div class="${note.title && "note-title"}">${note.title}</div>
         <div class="note-text">${note.text}</div>
@@ -81,7 +93,8 @@ class App {
         </div>
       </div>
     `
-    ).join(''); //this removes the commas by forming one string
+      )
+      .join(""); //this removes the commas by forming one string
   }
 }
 
