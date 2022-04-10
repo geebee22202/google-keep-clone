42649c71206f0d377bb40987cb7b7009d6111bc1
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-04-10 13:18:23 -0400

    added ability to change note color

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ class App {
     this.$modalTitle = document.querySelector(".modal-title");
     this.$modalText = document.querySelector(".modal-text");
     this.$modalCloseButton = document.querySelector(".modal-close-button");
+    this.$colorTooltip = document.querySelector("#color-tooltip");
 
     this.addEventListeners();
   }
@@ -24,6 +25,30 @@ class App {
       this.handleFormClick(event);
       this.selectNote(event);
       this.openModal(event);
+    });
+
+    document.body.addEventListener("mouseover", (event) => {
+      this.openTooltip(event);
+    });
+
+    document.body.addEventListener("mouseout", (event) => {
+      this.closeTooltip(event);
+    });
+
+    this.$colorTooltip.addEventListener("mouseover", function () {
+      this.style.display = "flex";
+    });
+
+    this.$colorTooltip.addEventListener("mouseout", function () {
+      this.style.display = "none";
+    });
+
+    this.$colorTooltip.addEventListener("mouseout", (event) => {
+      const color = event.target.dataset.color;
+
+      if (color) {
+        this.editNoteColor(color);
+      }
     });
 
     this.$form.addEventListener("submit", (event) => {
@@ -90,6 +115,23 @@ class App {
     this.$modal.classList.toggle("open-modal");
   }
 
+  //nextElementSibling allows us to get the dataset id from any element in the note div
+  //getBoundingClientRect let's us know exactly where the user is hovering on note
+  openTooltip(event) {
+    if (!event.target.matches(".toolbar-color")) return;
+    this.id = event.target.dataset.id;
+    const noteCoords = event.target.getBoundingClientRect();
+    const horizontal = noteCoords.left + window.scrollX;
+    const vertical = noteCoords.top + window.scrollY - 280;
+    this.$colorTooltip.style.transform = `translate(${horizontal}px, ${vertical}px)`;
+    this.$colorTooltip.style.display = "flex";
+  }
+
+  closeTooltip(event) {
+    if (!event.target.matches(".toolbar-color")) return;
+    this.$colorTooltip.style.display = "none";
+  }
+
   addNote({ title, text }) {
     const newNote = {
       title: title,
@@ -107,6 +149,13 @@ class App {
     const text = this.$modalText.value;
     this.notes = this.notes.map((note) =>
       note.id === Number(this.id) ? { ...note, title, text } : note
+    );
+    this.displayNotes();
+  }
+
+  editNoteColor(color) {
+    this.notes = this.notes.map((note) =>
+      note.id === Number(this.id) ? { ...note, color } : note
     );
     this.displayNotes();
   }
@@ -132,7 +181,7 @@ class App {
         <div class="note-text">${note.text}</div>
         <div class="toolbar-container">
           <div class="toolbar">
-            <img class="toolbar-color" src="https://icon.now.sh/palette" />
+            <img class="toolbar-color" data-id=${note.id} src="https://icon.now.sh/palette" />
             <img class="toolbar-delete" src="https://icon.now.sh/delete" />
           </div>
         </div>
